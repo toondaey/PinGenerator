b191abee1be46f32875a260b0b4b28767c04dd7b
Tunde <tunde@cchubnigeria.com>
2017-08-10 17:19:35 +0100

    Modifications made for faster number generation.

--- src/NumberGenerator/NumberGenerator.php
+++ src/NumberGenerator/NumberGenerator.php
@@ -2,8 +2,11 @@
 
 namespace NumberGenerator;
 
-use RuntimeException;
-
+/**
+ * 1-10 digits Pin generator.
+ *
+ * @author 'Tunde <tunde1990@gmail.com>'
+ */
 class NumberGenerator
 {
     protected static $chars;
@@ -14,48 +17,113 @@ class NumberGenerator
 
     protected static $includeZero = true;
 
-    protected static $range = [0, 9];
+    protected static $range;
 
-    function __construct(int $chars = 6, bool $repeating = true, bool $includeZero = true)
+    /**
+     * Contructor setting the initial number of characters to expect.
+     *
+     * @param integer $chars
+     */
+    function __construct(int $chars = 6)
     {
-        if ($chars < 1)
-            throw new RuntimeException('Invalid number', 400);
-
-        static::$chars = $chars;
-
-        static::$repeating = $repeating;
-
-        static::$includeZero = $includeZero;
+        static::setRange();
+        static::setChars($chars);
     }
 
+    protected static function setRange()
+    {
+        static::$range = range(0, 9);
+    }
+
+    /**
+     * Set the number of characters that will be present in the pin.
+     *
+     * @param int $chars Number of characters.
+     */
+    protected static function setChars(int $chars)
+    {
+        if ($chars < 1)
+            static::$chars = 1;
+
+        else if ($chars > 10)
+            static::$chars = 10;
+
+        else static::$chars = $chars;
+    }
+
+    /**
+     * Set repeating characters if characters can be repeated.
+     *
+     * @param boolean $repeating
+     */
+    static function repeatCharacters(bool $repeating = true)
+    {
+        static::$repeating = $repeating;
+    }
+
+    /**
+     * If you want zero to be included.
+     * Setting this to 'false' will make the number of characters that can be
+     * generated less than 10.
+     *
+     * @param boolean $includeZero [description]
+     */
+    static function includeZero(bool $includeZero = true)
+    {
+        if (! $includeZero)
+            static::remove(0);
+    }
+
+    /**
+     * Run the generator. Returns string to accomodate a zero-leading digits.
+     *
+     * @return string String of pin.
+     */
     static function generate ()
     {
         while (count(static::$numbers) < static::$chars)
         {
             $number = static::number();
 
-            if (static::check($number))
-                array_push(static::$numbers, $number);
+            array_push(static::$numbers, $number);
+            if (! static::$repeating)
+                static::remove($number);
         }
 
-        return json_encode(static::combine());
+        return static::combine();
     }
 
+    /**
+     * Generate a new number.
+     *
+     * @return int
+     */
     protected static function number ()
     {
-        return mt_rand(static::$range[0], static::$range[1]);
+        return array_rand(static::$range);
     }
 
+    /**
+     * Combine array of generated numbers into a single string.
+     *
+     * @return string
+     */
     protected static function combine ()
     {
-        return implode('', static::$numbers);
+        return (string) implode('', static::$numbers);
     }
 
-    protected static function check(int $number)
+    /**
+     * Remove characters if the repeating is set to false,
+     * or zero if it is not included.
+     *
+     * @param  int    $number
+     * @return void
+     */
+    protected static function remove(int $number)
     {
-        return (
-            (static::$repeating ? static::$repeating : ! in_array($number, static::$numbers)) &&
-            (static::$includeZero ? static::$includeZero : in_array(0, static::$numbers))
-        );
+        $key = array_search($number, static::$range);
+        unset(static::$range[$key]);
+        array_values(static::$range);
     }
 }
